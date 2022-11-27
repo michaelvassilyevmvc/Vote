e3e47d656a80e833f78781eccca774af9daa5a6e
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-11-28 00:11:28 +0600

    create domain

---
+++ Vote.Domain/Entities/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Vote.Domain.Entities
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ Vote.Domain/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿using Vote.Domain.Enums;
+
+namespace Vote.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public Category Category { get; set; }
+    }
+}
