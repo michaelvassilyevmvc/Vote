51e02f9ab9003fc97a7890a5d5587eb0463d3743
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2022-11-28 13:36:32 +0600

    Create behaviors and exception handlers

---
+++ Vote.Application/Behaviors/LogginBehavior.cs
@@ -0,0 +1,22 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vote.Application.Behaviors
+{
+    public class LogginBehavior<TRequest> : IRequestPreProcessor<TRequest>
+    {
+        private readonly ILogger<TRequest> _logger;
+
+        public LogginBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Process(TRequest request, CancellationToken cancellationToken)
+        {
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("Vote Request: {@Request}", requestName, request);
+        }
+    }
+}
---
+++ Vote.Application/Behaviors/PerformanceBehavior.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vote.Application.Behaviors
+{
+    public class PerformanceBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly ILogger<TRequest> _logger;
+        private readonly Stopwatch _timer;
+
+        public PerformanceBehavior(ILogger<TRequest> logger)
+        {
+            _logger = logger;
+            _timer = new Stopwatch();
+        }
+
+
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            _timer.Start();
+            var response = await next();
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.ElapsedMilliseconds;
+            if (elapsedMilliseconds <= 500) { return response; }
+
+            var requestName = typeof(TRequest).Name;
+            _logger.LogWarning("Vote Long Request: {Name} ({ElapsedMilliseconds} milliseconds) {@Request}", requestName, elapsedMilliseconds, request);
+            return response;
+        }
+    }
+}
